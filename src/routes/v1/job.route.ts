e1998b2d73d7c64597a96bbe97fac9882fd6aadc
Imanuel Urip Subekti <imanuel.subekti@efishery.com>
2023-10-27 17:30:41 +0700

    fix job

--- src/routes/v1/job.route.ts
+++ src/routes/v1/job.route.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { startJob, stopJob } from "../../utils/job";
+import { startJob, stopJob, statusJob } from "../../utils/job";
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ router.get("/start", (req: Request, res: Response) => {
   const data = startJob(start as string, end as string);
   res.json({
     success: true,
-    data
+    data,
   });
 });
 
@@ -16,7 +16,15 @@ router.get("/stop", (req: Request, res: Response) => {
   stopJob();
   res.json({
     success: true,
-    message: "job stopped"
+    message: "job stopped",
+  });
+});
+
+router.get("/status", (req: Request, res: Response) => {
+  const data = statusJob();
+  res.json({
+    success: true,
+    data,
   });
 });
 
--- src/utils/job.ts
+++ src/utils/job.ts
@@ -5,33 +5,38 @@ import { checkin, checkout } from "../services/darwin.service";
 let jobClockIn: cron.ScheduledTask | null = null;
 let jobClockOut: cron.ScheduledTask | null = null;
 
+let cronIn: string | null;
+let cronOut: string | null;
+
 export const startJob = (start: string, end: string) => {
   stopJob();
-  const defaultStart = "0 2 * * *"; // 09:00 | UTC + 7
-  const defaultEnd = "0 11 * * *"; // 18:00 | UTC + 7
-  jobClockIn = cron.schedule(start || defaultStart, async function () {
+  cronIn = start || "0 9 * * *"; // 09:00 | UTC + 7
+  cronOut = end || "0 18 * * *"; // 18:00 | UTC + 7
+  jobClockIn = cron.schedule(cronIn, async function () {
+    console.log(`[${new Date()}]: run job clockin`);
     checkin({
       location_type: 2,
       location: "Pavilliun 250, Gumuruh.",
       latlng: "-6.9314813,107.6377282",
       message: "",
     });
-    // console.log("in", new Date());
+    console.log("in", new Date());
   });
-  jobClockOut = cron.schedule(end || defaultEnd, async function () {
+  jobClockOut = cron.schedule(cronOut, async function () {
+    console.log(`[${new Date()}]: run job clockout`);
     checkout({
       location_type: 2,
       location: "Pavilliun 250, Gumuruh.",
       latlng: "-6.9314813,107.6377282",
       message: "",
     });
-    // console.log("out", new Date());
+    console.log("out", new Date());
   });
   jobClockIn.start();
   jobClockOut.start();
   return {
-    start: start || defaultStart,
-    end: end || defaultEnd,
+    start: cronIn,
+    end: cronOut,
   };
 };
 
@@ -39,9 +44,18 @@ export const stopJob = () => {
   if (!!jobClockIn) {
     jobClockIn.stop();
     jobClockIn = null;
+    cronIn = null;
   }
   if (!!jobClockOut) {
     jobClockOut.stop();
     jobClockOut = null;
+    cronOut = null;
   }
+};
+
+export const statusJob = () => {
+  return {
+    start: cronIn,
+    end: cronOut,
+  };
 };
