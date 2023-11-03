b90d092f8fcb01519ccd699c6821b071186d7840
Urip Subekti <urip.subekti13@gmail.com>
2023-11-03 08:34:42 +0700

    feat: add toast on error

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,3 +1,4 @@
+import { createStandaloneToast } from "@chakra-ui/react";
 import Axios from "axios";
 
 export const TOKEN_KEY = "darwin_token";
@@ -5,12 +6,30 @@ export const getToken = () => sessionStorage.getItem(TOKEN_KEY);
 export const setToken = (token: string) =>
   sessionStorage.setItem(TOKEN_KEY, token);
 
+const toast = createStandaloneToast();
 const axiosInstance = () =>
   Axios.create({
     baseURL: `${import.meta.env.VITE_API_HOST || ""}/v1`,
     headers: {
       ...(!!getToken() && { Authorization: `Bearer ${getToken()}` }),
     },
-  });
+  }).interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    (err) => {
+      let errMsg = err?.response?.data?.message;
+      if (!errMsg && err?.response?.data) {
+        try {
+          errMsg = JSON.stringify(err?.response?.data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      if (!errMsg) errMsg = err.message;
+      toast.toast({ title: errMsg, status: 'error' });
+      return Promise.reject(err);
+    }
+  );
 
 export default axiosInstance;
